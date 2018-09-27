a175ab57f700f4d63da354d0f5457bdb27969c1f
eaglejs <joshuaseagle@gmail.com>
2018-09-27 11:31:59 -0400

    Created Garage Component

---
+++ src/app/garages/garages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-garages',
+  templateUrl: './garages.component.html',
+  styleUrls: ['./garages.component.scss']
+})
+export class GaragesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
