c144322b1b615f1ccafe4d3581a75af93c8fd69b
eaglejs <joshuaseagle@gmail.com>
2018-09-27 11:31:23 -0400

    added user class and service

---
+++ src/app/user.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  user = {};
+  isRegistered = false;
+
+  constructor(private http: HttpClient) {}
+
+  checkLogin() {
+    return this.http({
+      method: 'post',
+      url: '/rest/isLoggedIn'
+    }).then( (response) => {
+      if (response.data.username) {
+        return response.data;
+      } else {
+        return false;
+      }
+    }, (error) => {
+      console.log(error);
+      return false
+    });
+  }
+
+  checkInitialRegistration() {
+    return this.http({
+      method: 'post',
+      url: '/rest/isRegistered'
+    }).then( response => {
+      this.isRegistered = response.data.isRegistered;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getUserInformation() {
+    return this.http({
+      method: 'post',
+      url: '/rest/getUserInformation'
+    }).then( response => {
+      this.user = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  setUser(user) {
+    this.user = user;
+  }
+
+  getUser() {
+    return this.user;
+  }
+}
---
+++ src/app/user.ts
@@ -0,0 +1,7 @@
+export class User {
+  email: string;
+  name: string;
+  username: string;
+  password: string;
+  role: string;
+}
