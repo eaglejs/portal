c8210b2076673a79058bd46967e8bcad0cb6e73f
eaglejs <joshuaseagle@gmail.com>
2018-10-02 14:30:17 -0400

    building out navigation with material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -7,6 +8,46 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { GaragesComponent } from './garages/garages.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatSliderModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,13 +55,54 @@ import { RegisterComponent } from './register/register.component';
     DashboardComponent,
     GaragesComponent,
     LoginComponent,
+    ReactiveFormsModule,
     RegisterComponent
   ],
+  exports: [
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule
+  ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,43 +10,43 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  checkLogin() {
-    return this.http({
-      method: 'post',
-      url: '/rest/isLoggedIn'
-    }).then( (response) => {
-      if (response.data.username) {
-        return response.data;
-      } else {
-        return false;
-      }
-    }, (error) => {
-      console.log(error);
-      return false
-    });
-  }
+  // checkLogin() {
+  //   return this.http({
+  //     method: 'post',
+  //     url: '/rest/isLoggedIn'
+  //   }).then( (response) => {
+  //     if (response.data.username) {
+  //       return response.data;
+  //     } else {
+  //       return false;
+  //     }
+  //   }, (error) => {
+  //     console.log(error);
+  //     return false
+  //   });
+  // }
 
-  checkInitialRegistration() {
-    return this.http({
-      method: 'post',
-      url: '/rest/isRegistered'
-    }).then( response => {
-      this.isRegistered = response.data.isRegistered;
-    }, error => {
-      console.log(error);
-    });
-  }
+  // checkInitialRegistration() {
+  //   return this.http({
+  //     method: 'post',
+  //     url: '/rest/isRegistered'
+  //   }).then( response => {
+  //     this.isRegistered = response.data.isRegistered;
+  //   }, error => {
+  //     console.log(error);
+  //   });
+  // }
 
-  getUserInformation() {
-    return this.http({
-      method: 'post',
-      url: '/rest/getUserInformation'
-    }).then( response => {
-      this.user = response.data;
-    }, error => {
-      console.log(error);
-    });
-  }
+  // getUserInformation() {
+  //   return this.http({
+  //     method: 'post',
+  //     url: '/rest/getUserInformation'
+  //   }).then( response => {
+  //     this.user = response.data;
+  //   }, error => {
+  //     console.log(error);
+  //   });
+  // }
 
   setUser(user) {
     this.user = user;
