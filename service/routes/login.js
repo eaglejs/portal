7444949866005445d9eabf262e27a57b1684c9e3
eaglejs <joshuaseagle@gmail.com>
2019-01-23 13:06:21 -0500

    updated formatting.

--- service/routes/login.js
+++ service/routes/login.js
@@ -4,73 +4,74 @@ var User = require('../models/user');
 
 router.get('/test', (req, res, next) => {
   res.status(200).json({
-    "success": true
+    success: true
   });
 });
 
 // POST /login
-router.post('/login', function (req, res, next) {
-
-    if (!!(req.body.username && req.body.password)) {
-        User.authenticate(req.body.username, req.body.password, function (error, user) {
-            if (error || !user) {
-                res.status(403).json({
-                    error: error
-                });
-            } else {
-                req.session.userId = user._id;
-                res.json({
-                    username: user.username,
-                    name: user.name,
-                    email: user.email,
-                    role: user.role
-                });
-            }
+router.post('/login', function(req, res, next) {
+  if (!!(req.body.username && req.body.password)) {
+    User.authenticate(req.body.username, req.body.password, function(
+      error,
+      user
+    ) {
+      if (error || !user) {
+        res.status(403).json({
+          error: error
         });
-    } else {
-        var err = new Error('Email and password are required.');
-        err.status = 401;
-        return next(err);
-    }
+      } else {
+        req.session.userId = user._id;
+        res.json({
+          username: user.username,
+          name: user.name,
+          email: user.email,
+          role: user.role
+        });
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
 });
 
 // GET /logout
-router.get('/logout', function (req, res, next) {
-    if (req.session) {
-        // devare session object
-        req.session.destroy(function (err) {
-            if (err) {
-                return next(err);
-            } else {
-                return res.status(200).json({ loggedIn: false });
-            }
-        });
-    }
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // devare session object
+    req.session.destroy(function(err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.status(200).json({ loggedIn: false });
+      }
+    });
+  }
 });
 
 // POST /isLoggedIn
-router.post('/is-logged-in', function (req, res, next) {
-    if (req.session && req.session.userId) {
-        User.findById(req.session.userId)
-            .exec(function (error, user) {
-                if (error) {
-                    res.json({
-                        message: 'You must be logged in to access the application.'
-                    });
-                } else {
-                    res.json({
-                        username: user.username,
-                        name: user.name,
-                        email: user.email,
-                        role: user.role
-                    });
-                }
-            });
-    } else {
-        res.send({
-            error: "You must be logged in to access the application"
+router.post('/is-logged-in', function(req, res, next) {
+  if (req.session && req.session.userId) {
+    User.findById(req.session.userId).exec(function(error, user) {
+      if (error) {
+        res.json({
+          message: 'You must be logged in to access the application.'
         });
-    }
+      } else {
+        res.json({
+          username: user.username,
+          name: user.name,
+          email: user.email,
+          role: user.role
+        });
+      }
+    });
+  } else {
+    res.send({
+      error: 'You must be logged in to access the application'
+    });
+  }
 });
 
 module.exports = router;
