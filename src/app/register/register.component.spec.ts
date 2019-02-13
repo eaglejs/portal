e08f9760266dcd7ced4ef4f6fadc45f05b12d599
eaglejs <joshuaseagle@gmail.com>
2019-02-13 17:06:42 -0500

    Added dependencies to register spec.

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,4 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../material';
+import {
+  FormsModule,
+  FormBuilder,
+  ReactiveFormsModule
+} from '@angular/forms';
 
 import { RegisterComponent } from './register.component';
 
@@ -8,9 +16,10 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterComponent],
+      imports: [FormsModule, MaterialModule, BrowserModule, BrowserAnimationsModule, ReactiveFormsModule],
+      providers: [FormBuilder]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
