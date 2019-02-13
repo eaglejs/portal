b4bc53c331201ebdddba84aa35fb4f77af7308bf
eaglejs <joshuaseagle@gmail.com>
2019-02-13 15:30:12 -0500

    added working fake gpio, uncommented controller code

--- python/GarageController.py
+++ python/GarageController.py
@@ -7,30 +7,26 @@ import RPi.GPIO as GPIO
 # "state_pin": 17,
 # "state_pin_closed_value" : 0
 
+GPIO.setmode(GPIO.BOARD)
 
 class GarageController:
-    def __init__(self, selected_garage_door=0):
-        self.relay_pin = 23
-        self.state_pin = 17
-        self.state_pin_closed_value = 0
+	def __init__(self):
+		self.relay_pin = 23
+		self.state_pin = 17
+		self.state_pin_closed_value = 0
 
-        # GPIO.setup(self.relay_pin, GPIO.OUT)
-        # GPIO.setup(self.state_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # GPIO.output(self.relay_pin, True)
-        sys.stdout.flush()
+		GPIO.setup(self.relay_pin, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
+		GPIO.setup(self.state_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		GPIO.output(self.relay_pin, True)
+		sys.stdout.flush()
 
-    def get_state(self):
-        return True
-        # if GPIO.input(self.state_pin) == self.state_pin_closed_value:
-        #     return True
-        # else:
-        #     return False
+	def get_state(self):
+		if GPIO.input(self.state_pin) == self.state_pin_closed_value:
+		    return True
+		else:
+		    return False
 
-    def toggle_relay(self):
-        print('hello')
-        # GPIO.output(self.relay_pin, False)
-        # GPIO.output(self.relay_pin, True)
-
-
-# if __name__ == '__main__':
-#     gc = GarageController()
+	def toggle_relay(self):
+		print('hello')
+		GPIO.output(self.relay_pin, False)
+		GPIO.output(self.relay_pin, True)
--- python/RPi/GPIO.py
+++ python/RPi/GPIO.py
@@ -1,23 +1,23 @@
 BOARD = 1
 OUT = 1
 IN = 1
+PUD_UP = 1
 
 
 def setmode(a):
-    print(a)
+	print(a)
 
-
-def setup(a, b):
-    print(a)
-
+def setup(a, b, pull_up_down, *args, **kwargs):
+	print(a)
 
 def output(a, b):
-    print(a)
+	print(a)
 
+def input(a):
+	return 0
 
 def cleanup():
-    print('a')
-
+	print('a')
 
 def setwarnings(flag):
-    print('False')
+	print('False')
