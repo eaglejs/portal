b317e123325db9daa22eca5b3ecea4fcb5b065d6
eaglejs <joshuaseagle@gmail.com>
2019-02-13 17:06:11 -0500

    Added NG Forms.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerFormControl: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.registerFormControl = this.formBuilder.group({
+      confirmPassword: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      username: ['', [Validators.required]]
+    });
   }
 
+  getErrorMessage() {
+    return this.registerFormControl.get('email').hasError('required')
+      ? 'You must enter a value'
+      : this.registerFormControl.get('email').hasError('email')
+      ? 'Not a valid email'
+      : '';
+  }
 }
