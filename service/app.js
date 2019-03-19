e4bcbd364992e4f7914a078e8b8376d8c49ad2b4
eaglejs <joshuaseagle@gmail.com>
2019-03-19 11:26:44 -0400

    Updates to node services and libraries.

--- service/app.js
+++ service/app.js
@@ -16,29 +16,25 @@ const originsWhitelist = [
   'http://www.myproductionurl.com'
 ];
 const corsOptions = {
-  origin: function(origin, callback) {
+  origin: (origin, callback) => {
     const isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
     callback(null, isWhitelisted);
   }
 };
+const APP = express();
 
-const app = express();
+let db = null;
 
-// mongodb connection
-mongoose.connect(
-  'mongodb://mongo:27017/portal',
-  {}
-);
+// create mongodb connection
+mongoose.connect('mongodb://mongo:27017/portal', {});
 
-let db = mongoose.connection;
-// mongo error
-db.on('error', console.error.bind(console, 'connection error:'));
+db = mongoose.connection;
 
 //add cors whitelist
-app.use(cors(corsOptions));
+APP.use(cors(corsOptions));
 
 // user sessions for tracking logins
-app.use(
+APP.use(
   session({
     secret: "It's dangerous to go alone!",
     resave: true,
@@ -50,31 +46,30 @@ app.use(
 );
 
 // make user ID available in templates
-app.use(function(req, res, next) {
+APP.use((req, res, next) => {
   res.locals.currentUser = req.session.userId;
   next();
 });
 
 // parse incoming requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+APP.use(bodyParser.json());
+APP.use(bodyParser.urlencoded({ extended: false }));
 
 // include routes
-//app.use('/', routes);
-[].forEach.call(routes, function(route) {
-  app.use('/', route);
+routes.forEach(route => {
+  APP.use('/', route);
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+APP.use((req, res, next) => {
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-// define as the last app.use callback
-app.use(function(err, req, res, next) {
+// define as the last APP.use callback
+APP.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
     message: err.message,
@@ -83,6 +78,6 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-app.listen(port, function() {
-  console.log(`Express app listening on port: ${port}`);
+APP.listen(port, function() {
+  console.log(`Express APP listening on port: ${port}`);
 });
