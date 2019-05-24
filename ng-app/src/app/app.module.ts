aa6f238377c65c626c8ef3a26baa214a57b04e04
eaglejs <joshuaseagle@gmail.com>
2019-05-24 16:34:14 -0400

    Added flex layout.

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -1,15 +1,16 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
 
 @NgModule({
@@ -24,6 +25,7 @@ import { MaterialModule } from './material';
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    FlexLayoutModule,
     FormsModule,
     HttpClientModule,
     MaterialModule,
