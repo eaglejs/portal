98ae20ae6c1a693fc5a01466470287ceda9b69d8
eaglejs <joshuaseagle@gmail.com>
2019-07-30 11:00:36 -0400

    unused imports removed.

--- ng-app/src/app/components/login/login.component.ts
+++ ng-app/src/app/components/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { UserService } from '../user.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
