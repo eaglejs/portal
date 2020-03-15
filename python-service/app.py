141a6c12c366fe6d5d6a1609df0702cdf76d1e7d
eaglejs <joshuaseagle@gmail.com>
2020-03-15 14:44:53 -0400

    making python debuggable

--- python-service/app.py
+++ python-service/app.py
@@ -1,7 +1,11 @@
 #!flask/bin/python
 from flask import Flask, jsonify
 from GarageController import GarageController
-IPCONFIG = '0.0.0.0'
+
+IPCONFIG = "0.0.0.0"
+PORT = 5000
+DEBUGGER = True
+RELOADER = True
 
 app = Flask(__name__)
 
@@ -9,15 +13,18 @@ app = Flask(__name__)
 
 gc = GarageController()
 
-@app.route('/toggle-garage-door', methods=['GET'])
+
+@app.route("/toggle-garage-door", methods=["GET"])
 def toggleGarageDoor():
-	gc.toggleGarageDoor()
-	return jsonify({'door-status': gc.getGarageState()})
+    gc.toggleGarageDoor()
+    return jsonify({"door-status": gc.getGarageState()})
 
 
-@app.route('/garage-door-status', methods=['GET'])
+@app.route("/garage-door-status", methods=["GET"])
 def getGarageState():
-	return jsonify({'door-status': gc.getGarageState()})
+    return jsonify({"door-status": gc.getGarageState()})
 
-if __name__ == '__main__':
-	app.run(host=IPCONFIG, debug=True)
+
+if __name__ == "__main__":
+    app.run(host=IPCONFIG, port=PORT, debug=DEBUGGER, use_reloader=RELOADER)
+
